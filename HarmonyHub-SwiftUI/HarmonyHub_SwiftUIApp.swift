1bb1a0e0f18fade5d2162c551e60dd48327d9d39
ibrahimmbyrrm <ibrahim_bayram22@yahoo.com>
2023-09-22 14:44:19 +0300

    Initial Commit

---
+++ HarmonyHub-SwiftUI/HarmonyHub_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarmonyHub_SwiftUIApp.swift
+//  HarmonyHub-SwiftUI
+//
+//  Created by İbrahim Bayram on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HarmonyHub_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
